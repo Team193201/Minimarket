fb407c684d0b8c91d2c354b0f3d325f08446787e
Hossein Golmohammadi <42300369+Hosein201@users.noreply.github.com>
2022-07-13 23:27:22 +0430

    add product controller

---
+++ src/Minimarket/WebApi/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet("GetProduct")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return await  Task.FromResult(Ok());
+        }
+
+        [HttpGet("GetProducts")]
+        public async Task<IActionResult>Get(int task , int skip)
+        {
+            return await Task.FromResult(Ok());
+        }
+
+        [HttpPost("PostProduct")]
+        public async Task<IActionResult> Post()
+        {
+            return await Task.FromResult(Ok());
+        }
+
+        [HttpPut("PutProduct")]
+        public async Task<IActionResult>Put(int id)
+        {
+            return await Task.FromResult(Ok());
+        }
+
+        [HttpDelete("DeleteProduct")]
+        public async  Task<IActionResult> Delete(int id)
+        {
+            return await Task.FromResult(Ok());
+        }
+    }
+}
