34a7c93af2cc76ef74cb5d1aa731bd9dca72a899
[saharGhorbanzad] <[s.ghorbanzad@gmail.com]>
2022-07-14 21:14:55 +1300

    Add the classes Prodact And Categories in Model

---
+++ src/Minimarket/Entities/Model/Categories.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Categories
+    {
+        [Display(Name = "شناسه دسته"), Required]
+        public int CategoryID { get; set; }
+
+        [Display(Name = "نام دسته"), Required]
+        public string CategoryName { get; set; }
+
+        [Display(Name = "شرح"), Required]
+        public string Description { get; set; }
+
+        [Display(Name = "تصویر"), Required]
+        public byte[] Picture { get; set; }
+    }
+}
