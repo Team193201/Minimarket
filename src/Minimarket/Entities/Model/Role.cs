cbde3bb76fcaf017d809eb8bc3807b98d4538ab8
Hossein Golmohammadi <42300369+Hosein201@users.noreply.github.com>
2022-07-18 18:40:55 +0430

    DataAnnotations for user and role

--- src/Minimarket/Entities/Model/Role.cs
+++ src/Minimarket/Entities/Model/Role.cs
@@ -1,10 +1,12 @@
 ﻿using Entities.Interface;
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace Entities.Model
 {
     public class Role : IdentityRole<Guid>, IEntity
     {
+        [Display(Name = "شرح")]
         public string? Description { get; set; }
     }
 }
