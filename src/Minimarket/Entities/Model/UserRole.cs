b2b475e46942005b285c187664d9d8d1f613399a
saharGhorbanzad <93582923+saharGhorbanzad@users.noreply.github.com>
2022-07-21 06:05:47 +1300

    UserRol Configuration

--- src/Minimarket/Entities/Model/UserRole.cs
+++ src/Minimarket/Entities/Model/UserRole.cs
@@ -5,5 +5,7 @@ namespace Entities.Model
 {
     public class UserRole : IdentityUserRole<Guid>, IEntity
     {
+        public User User { get; set; }
+        public Role Role { get; set; }
     }
 }
