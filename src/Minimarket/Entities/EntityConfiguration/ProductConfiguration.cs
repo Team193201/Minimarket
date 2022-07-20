18840ffdc8f5b0ee9a28fc714fa9d8ba68bb3408
saharGhorbanzad <93582923+saharGhorbanzad@users.noreply.github.com>
2022-07-21 05:24:55 +1300

    product configuration

--- src/Minimarket/Entities/EntityConfiguration/ProductConfiguration.cs
+++ src/Minimarket/Entities/EntityConfiguration/ProductConfiguration.cs
@@ -1,14 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace Entities.Model
+namespace Entities.EntityConfiguration
 {
-    public class ProductMap : IEntityTypeConfiguration<Product>
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
     {
 
         public void Configure(EntityTypeBuilder<Product> builder)
         {
-           // builder.ToTable("","");
+            // builder.ToTable("","");
             builder.HasOne(c => c.category)
                 .WithMany(p => p.Products)
                 .HasForeignKey(p => p.CategoryID)
