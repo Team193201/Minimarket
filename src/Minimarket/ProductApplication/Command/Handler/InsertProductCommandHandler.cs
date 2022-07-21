c568c9a8cb3cfe297701fa329a7b3e39778b5cf2
Hossein Golmohammadi <42300369+Hosein201@users.noreply.github.com>
2022-07-21 21:13:14 +0430

    create dto for product

--- src/Minimarket/ProductApplication/Command/Handler/InsertProductCommandHandler.cs
+++ src/Minimarket/ProductApplication/Command/Handler/InsertProductCommandHandler.cs
@@ -1,21 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Infrastructure.Interface;
+﻿using Infrastructure.Interface;
 using MediatR;
+using ProductApplication.Dto;
 
 namespace ProductApplication.Command.Handler
 {
-    public class InsertProductCommandHandler : IRequestHandler<InsertProductCommand, List<string>>
+    public class InsertProductCommandHandler : IRequestHandler<InsertProductCommand, InsertProductDto>
     {
         private IUnitOfWork UnitOfWork;
         public InsertProductCommandHandler(IUnitOfWork unitOfWork)
         {
             UnitOfWork = unitOfWork;
         }
-        public Task<List<string>> Handle(InsertProductCommand request, CancellationToken cancellationToken)
+        public Task<InsertProductDto> Handle(InsertProductCommand request, CancellationToken cancellationToken)
         {
             // map InsertProductCommand to entity
             //rep add product 
@@ -23,14 +19,14 @@ namespace ProductApplication.Command.Handler
             // log 
             //event
 
-            UnitOfWork.CategoryRepository.AddAsyncEntity(new Entities.Category() { }, cancellationToken);
+            // UnitOfWork.CategoryRepository.AddAsyncEntity(new Entities.Category() { }, cancellationToken);
 
 
             // add product
             // add cat
             //update useer
 
-            UnitOfWork.SaveChangesAsync(cancellationToken);
+            //UnitOfWork.SaveChangesAsync(cancellationToken);
             //save 
             throw new NotImplementedException();
         }
