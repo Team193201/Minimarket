876f20f35b7ee437c753b8ffd27d284085013c75
Hossein Golmohammadi <42300369+Hosein201@users.noreply.github.com>
2022-07-22 14:20:54 +0430

    CategoryController

---
+++ src/Minimarket/WebApi/Controllers/CategoryController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+
+        [HttpGet("GetCategory")]
+        public async Task<IActionResult> Get(int id, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(Ok());
+        }
+
+        [HttpGet("GetCategorys")]
+        public async Task<IActionResult> Get(int task, int skip, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(Ok());
+        }
+
+        [HttpPost("PostCategory")]
+        public async Task<IActionResult> Post(CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(Ok());
+        }
+
+        [HttpPut("PutCategory")]
+        public async Task<IActionResult> Put(int id, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(Ok());
+        }
+
+        [HttpDelete("DeleteCategory")]
+        public async Task<IActionResult> Delete(int id, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(Ok());
+        }
+    }
+}
