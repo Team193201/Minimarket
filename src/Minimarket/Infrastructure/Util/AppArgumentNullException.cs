12143f2d4778b362327609129e3278add32b1767
Hossein Golmohammadi <42300369+Hosein201@users.noreply.github.com>
2022-08-28 12:37:26 +0430

    overlod on ThrowIfNull

---
+++ src/Minimarket/Infrastructure/Util/AppArgumentNullException.cs
@@ -0,0 +1,35 @@
+﻿namespace Infrastructure.Util
+{
+    public class AppArgumentNullException : ArgumentNullException
+    {
+        public static void ThrowIfNull(Guid argument,string paramName, string message = default, bool loggeError = false)
+        {
+            if (argument == Guid.Empty)
+                if (loggeError) { }
+            //TODO: set logg and throw exception
+
+            if (argument == Guid.Empty)
+                throw new ArgumentNullException(string.IsNullOrEmpty(message) ? $"this is ({paramName}) null" : message);
+        }
+
+        public static void ThrowIfNull(int argument, string paramName, string message = default, bool loggeError = false)
+        {
+            if (argument == default)
+                if (loggeError) { }
+            //TODO: set logg and throw exception
+
+            if (argument == default && !loggeError)
+                throw new ArgumentNullException(string.IsNullOrEmpty(message) ? $"this is ({paramName}) null" : message);
+        }
+
+        public static void ThrowIfNull(string argument, string paramName, string message = default, bool loggeError = false)
+        {
+            if (string.IsNullOrEmpty(argument))
+                if (loggeError) { }
+            //TODO: set logg and throw exception
+
+            if (string.IsNullOrEmpty(argument) && !loggeError)
+                throw new ArgumentNullException(string.IsNullOrEmpty(message) ? $"this is ({paramName}) null" : message);
+        }
+    }
+}
