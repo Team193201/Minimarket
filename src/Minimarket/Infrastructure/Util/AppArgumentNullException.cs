35b56a277fb5578771236d5a2082fa3ce876d7aa
Hossein Golmohammadi <42300369+Hosein201@users.noreply.github.com>
2022-08-28 21:50:15 +0430

    add NotNull and NotEmpty to AppArgumentNullException

--- src/Minimarket/Infrastructure/Util/AppArgumentNullException.cs
+++ src/Minimarket/Infrastructure/Util/AppArgumentNullException.cs
@@ -1,4 +1,6 @@
-﻿namespace Infrastructure.Util
+﻿using System.Collections;
+
+namespace Infrastructure.Util
 {
     public class AppArgumentNullException : ArgumentNullException
     {
@@ -9,7 +11,7 @@
             //TODO: set logg and throw exception
 
             if (argument == Guid.Empty)
-                throw new ArgumentNullException(string.IsNullOrEmpty(message) ? $"this is ({paramName}) null" : message);
+                throw new ArgumentNullException(string.IsNullOrEmpty(message) ? $"Argument is ({paramName}) empty" : message);
         }
 
         public static void ThrowIfNull(int argument, string paramName, string message = default, bool loggeError = false)
@@ -19,7 +21,7 @@
             //TODO: set logg and throw exception
 
             if (argument == default && !loggeError)
-                throw new ArgumentNullException(string.IsNullOrEmpty(message) ? $"this is ({paramName}) null" : message);
+                throw new ArgumentNullException(string.IsNullOrEmpty(message) ? $"Argument is ({paramName}) empty" : message);
         }
 
         public static void ThrowIfNull(string argument, string paramName, string message = default, bool loggeError = false)
@@ -29,7 +31,33 @@
             //TODO: set logg and throw exception
 
             if (string.IsNullOrEmpty(argument) && !loggeError)
-                throw new ArgumentNullException(string.IsNullOrEmpty(message) ? $"this is ({paramName}) null" : message);
+                throw new ArgumentNullException(string.IsNullOrEmpty(message) ? $"Argument is ({paramName}) empty" : message);
+        }
+
+        public static void NotNull<T>(T obj, string name, string message = null)
+           where T : class
+        {
+            if (obj is null)
+                throw new ArgumentNullException($"{name} : {typeof(T)}", message);
+        }
+
+        public static void NotNull<T>(T? obj, string name, string message = null)
+            where T : struct
+        {
+            if (!obj.HasValue)
+                throw new ArgumentNullException($"{name} : {typeof(T)}", message);
+
+        }
+
+        public static void NotEmpty<T>(T obj, string name, string message = null, T defaultValue = null)
+            where T : class
+        {
+            if (obj == defaultValue
+                || (obj is string str && string.IsNullOrWhiteSpace(str))
+                || (obj is IEnumerable list && !list.Cast<object>().Any()))
+            {
+                throw new ArgumentException("Argument is empty : " + message, $"{name} : {typeof(T)}");
+            }
         }
     }
 }
