58c279163391802fda9081275f023ddfdf445b08
Hossein Golmohammadi <42300369+Hosein201@users.noreply.github.com>
2022-09-15 21:42:01 +0430

    Application Setting for ApiGetWay

---
+++ src/Minimarket/ApiGetWay/Infrastructure/ApplicationSetting.cs
@@ -0,0 +1,25 @@
+﻿namespace ApiGetWay.Infrastructure
+{
+    public class ApplicationSetting
+    {
+        public LoggSetting Logg { get; set; }
+    }
+
+    public class LoggSetting
+    {
+        public string EnvironmentName { get; set; }
+        public ConsoleStting ConsoleStting { get; set; }
+        public ElasticsearchSetting ElasticSetting { get; set; }
+    }
+
+    public class ConsoleStting
+    {
+        public string OutputTemplate { get; set; }
+    }
+
+    public class ElasticsearchSetting
+    {
+        public string Url { get; set; }
+        public string FileNameFormat { get; set; }
+    }
+}
