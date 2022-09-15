11d2e51ae9a1c361ee46473a682974a6862bdb05
Hossein Golmohammadi <42300369+Hosein201@users.noreply.github.com>
2022-09-15 21:43:23 +0430

    serilog

--- src/Minimarket/ApiGetWay/Program.cs
+++ src/Minimarket/ApiGetWay/Program.cs
@@ -1,35 +1,37 @@
+using ApiGetWay.Infrastructure;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Serilog;
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-var configuration = new ConfigurationBuilder()
-   .SetBasePath(builder.Environment.ContentRootPath)
-   .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
-   .AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", optional: true)
-   .AddEnvironmentVariables().Build();
+var appSetting = builder.Configuration.GetSection(nameof(ApplicationSetting)).Get<ApplicationSetting>();
 
 //--------------------------- Services --------------------------------
 // Add services to the container.
 
+builder.Host.UseSerilog(CustomLoggerConfiguration.CustomCreateLogger(appSetting.Logg));
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddOcelot(configuration);
-
+builder.Services.AddOcelot(CustomOcelotBuilder.Builder(builder.Environment));
+builder.Services.AddSingleton(Log.Logger);
 var app = builder.Build();
 
 
 //--------------------------- Configure --------------------------------
 //-----------------------------------------------------------
 // Configure the HTTP request pipeline.
+
+app.UseSerilogRequestLogging();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-
 app.UseHttpsRedirection();
 app.UseRouting();
 app.UseOcelot();
