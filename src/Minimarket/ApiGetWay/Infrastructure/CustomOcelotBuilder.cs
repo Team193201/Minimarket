71cbed3b56a218247197e4dc699d36fb35b783af
Hossein Golmohammadi <42300369+Hosein201@users.noreply.github.com>
2022-09-15 21:42:55 +0430

    CustomLoggerConfiguration && CustomOcelotBuilder

---
+++ src/Minimarket/ApiGetWay/Infrastructure/CustomOcelotBuilder.cs
@@ -0,0 +1,17 @@
+﻿using Serilog;
+
+namespace ApiGetWay.Infrastructure
+{
+    public static class CustomOcelotBuilder
+    {
+        public static IConfigurationRoot Builder(IWebHostEnvironment webHostEnvironment)
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(webHostEnvironment.ContentRootPath)
+                .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"ocelot.{webHostEnvironment.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+        }
+    }
+}
